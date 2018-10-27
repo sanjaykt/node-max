0cd9b04fbcd7009c8a21791ad3e2028e1a3ef7ec
Sanjay Tailor <st.sanjaytailor@gmail.com>
2018-10-27 13:05:19 +0530

    added static path to the public folder using express.static and than path

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use((req, res, next) => {
    // res.send('<html><body><h1>Not found</h1></body></html>')
-   res.sendFile(path.join(rootDir, 'views', 'error.html'));
+   res.sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
